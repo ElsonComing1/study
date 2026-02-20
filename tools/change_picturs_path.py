df1c9a67b9e3a1086793dd095d50052dff5f516c
ElsonComing1 <1653195118@qq.com>
2026-02-20 11:29:17 +0800

    2026/2/20 linux指令学习

--- tools/change_picturs_path.py
+++ tools/change_picturs_path.py
@@ -1,4 +1,5 @@
-import os
+import os,ast
+
 
 with open(r"C:\Users\16531\Desktop\study\daily_logs\2026-02-19.md",'r',encoding='utf-8') as file:
     # content=file.read()
@@ -7,6 +8,6 @@ with open(r"C:\Users\16531\Desktop\study\daily_logs\2026-02-19.md",'r',encoding=
         os.remove(r"C:\Users\16531\Desktop\study\daily_logs\2026-02-20.md")
     # print(repr(content))
     for line in content:
-        every_line=line
+        every_line=repr(line)
         with open(r"C:\Users\16531\Desktop\study\daily_logs\2026-02-20.md",'a+',encoding='utf-8') as file1:
-            file1.write(every_line)
+            file1.write(ast.literal_eval(every_line))
